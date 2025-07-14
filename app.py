a98eb5cfcc725bb82ead07596122386a1ef11d14
bastidas1010 <70337092+bastidas1010@users.noreply.github.com>
2025-07-14 11:57:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1480,4 +1480,5 @@ def create_custom_song():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
